c9cdc79e9bf029f3dc9e44eb41a395b9d5ac5a99
emrako <55356758+emrako@users.noreply.github.com>
2019-09-15 09:13:53 -0700

    Create sync.py

---
+++ sync.py
@@ -0,0 +1,70 @@
+# age = int(input("What's your age Sire. "))
+#
+# if age >= 18 and age <= 25:
+#     print("You are welcome! Drinks are on the house!")
+# else:
+#     print("Sorry, We can't offer you any drinks.")
+
+#######################
+
+# x = 0
+#
+# while x <= 5:
+#     print("Current value of x is ", x)
+#     x = x+1
+# else:
+#     print("Loop completed")
+#
+#######################
+#  WRONG LOOP
+# user_input = int(input("Please enter a number: "))
+# new_input = int(input("What to add another number? Please enter"))
+# while new_input != 0:
+#     user_input = user_input + new_input
+#     print(user_input)
+#     # print(user_input + new_input)
+#     # print(new_input)
+# else:
+#     print("Done!")
+
+#######################
+# num = 1
+# sum = 0
+# print("Enter a number to add, Press '0' to exit ")
+#
+# while num != 0:
+#     num = int(input("Enter a number to add: "))
+#     sum = num + sum
+#     print("Total sum is: ", sum)
+#
+# else:
+#     print("Loop completed")
+#######################
+# A = [1, 2, 3, 4, 5] #list
+# B = (1, 2, 3, 4, 5,) #Tuple
+# C = {1, 2, 3, 4, 5} #set
+# D = 'Raghav' #string
+# E = {'name': 'Raghav',
+#      'age': 27} #Dictionary
+#
+# for x, y in E.items():
+#     print(x, y)
+#######################
+# a = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+# for i in a:
+#     if i == 5:
+#         # break
+#         continue
+#     print(i)
+#######################
+#
+i = 0
+while i < 9:
+    i += 1
+    if i == 5:
+        continue
+        # break
+    print(i)
+
+else:
+    print("Loop completed")
